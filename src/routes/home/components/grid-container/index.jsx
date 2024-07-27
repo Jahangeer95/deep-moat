6477178a3ee936f046358c0d29d1c87b48c262fb
Jahangeer95 <alijahangir95@gmail.com>
2024-07-27 13:12:33 +0500

    random letters functionality added.

--- src/routes/home/components/grid-container/index.jsx
+++ src/routes/home/components/grid-container/index.jsx
@@ -1,9 +1,93 @@
-import React, { useState } from "react";
-import { gridInitialState } from "../../constants";
+import React, { useEffect, useState } from "react";
+import { gridInitialState, lettersObj } from "../../constants";
 import styles from "../../styles.module.css";
 
-export function GridContainer() {
+export function GridContainer({ randomLetters }) {
   const [grid, setGrid] = useState(gridInitialState);
+
+  useEffect(() => {
+    const letter1 = lettersObj[randomLetters[0]];
+    const letter2 = lettersObj[randomLetters[1]];
+    setGrid(fillGridWithPattern(letter1, letter2));
+  }, [randomLetters]);
+
+  function fillGridWithPattern(pattern1, pattern2) {
+    const patternRows = pattern1?.length || 0;
+    const patternCols = pattern1[0]?.length || 0;
+
+    let firstLetterRowStart = 2;
+    let firstLetterColStart = 3;
+    let secondLetterRowStart = 13;
+    let sedondLetterColStart = 15;
+
+    return grid.map((row, rowIdx) => {
+      return row.map((cell, colIdx) => {
+        if (
+          rowIdx >= firstLetterRowStart &&
+          rowIdx < firstLetterRowStart + patternRows &&
+          colIdx >= firstLetterColStart &&
+          colIdx < firstLetterColStart + patternCols
+        ) {
+          return pattern1[rowIdx - firstLetterRowStart][
+            colIdx - firstLetterColStart
+          ] === 1
+            ? true
+            : false;
+        }
+        if (
+          rowIdx >= secondLetterRowStart &&
+          rowIdx < secondLetterRowStart + patternRows &&
+          colIdx >= sedondLetterColStart &&
+          colIdx < sedondLetterColStart + patternCols
+        ) {
+          return pattern2[rowIdx - secondLetterRowStart][
+            colIdx - sedondLetterColStart
+          ] === 1
+            ? true
+            : false;
+        }
+        return cell;
+      });
+    });
+  }
+
+  const handleDrop = (e, row, col) => {
+    const { row: startRow, col: startCol } = JSON.parse(
+      e.dataTransfer.getData("text/plain")
+    );
+    setGrid((prevGrid) => {
+      const deepCloneGrid = JSON.parse(JSON.stringify(prevGrid));
+      const temp = prevGrid[row][col];
+      deepCloneGrid[row][col] = prevGrid[startRow][startCol];
+      deepCloneGrid[startRow][startCol] = temp;
+      return deepCloneGrid;
+    });
+  };
+
+  const handleDragStart = (e, row, col) => {
+    e.dataTransfer.setData("text/plain", JSON.stringify({ row, col }));
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  function getGridItemClasses(colNo, rowNo, arrayLength, item) {
+    let classes = [""];
+    if (colNo === arrayLength - 1) {
+      classes.push(styles.borderRight);
+    }
+
+    if (rowNo === arrayLength - 1) {
+      classes.push(styles.borderBottom);
+    }
+
+    if (item) {
+      classes.push(styles.fill);
+    }
+
+    return classes.join(" ");
+  }
 
   return (
     <div className={styles.gridContainer}>
@@ -12,8 +96,10 @@ export function GridContainer() {
           <GridItem
             key={rowIdx + colIdx}
             state={item}
-            isBorderRight={colIdx === state.length - 1}
-            isBorderBottom={rowIdx === state.length - 1}
+            classes={getGridItemClasses(colIdx, rowIdx, state.length, item)}
+            onDragStart={(e) => handleDragStart(e, rowIdx, colIdx)}
+            onDrop={(e) => handleDrop(e, rowIdx, colIdx)}
+            onDragOver={handleDragOver}
           />
         ));
       })}
@@ -21,12 +107,14 @@ export function GridContainer() {
   );
 }
 
-function GridItem({ state, isBorderRight, isBorderBottom }) {
+function GridItem({ classes, onDragStart, onDragOver, onDrop }) {
   return (
     <div
-      className={`${styles.gridItem} ${
-        isBorderRight ? styles.borderRight : ""
-      } ${isBorderBottom ? styles.borderBottom : ""}`}
+      className={`${styles.gridItem} ${classes}`}
+      draggable
+      onDragStart={onDragStart}
+      onDragOver={onDragOver}
+      onDrop={onDrop}
     />
   );
 }
--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { GridContainer } from "./components";
 import styles from "./styles.module.css";
+import { lettersObj } from "./constants";
 
 export function HomeContent() {
+  const [randomChars, setRandomChars] = useState(["A", "C"]);
+
+  const getRandomChars = () => {
+    const lettersArr = Object.keys(lettersObj);
+
+    function getRandomIndex() {
+      return Math.floor(Math.random() * lettersArr.length);
+    }
+
+    const char1 = lettersArr[getRandomIndex()];
+    const char2 = lettersArr[getRandomIndex()];
+
+    setRandomChars([char1, char2]);
+  };
+
   return (
     <main className={styles.mainContainer}>
-      <button type="button">Generate</button>
-      <GridContainer />
+      <button type="button" onClick={getRandomChars}>
+        Generate
+      </button>
+      <GridContainer randomLetters={randomChars} />
     </main>
   );
 }
